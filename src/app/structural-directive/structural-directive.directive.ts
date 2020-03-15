4104af2c07f7a27875c3a738932f1750d03beb28
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2020-03-15 20:17:41 +0530

    Added comments for structural directive

--- src/app/structural-directive/structural-directive.directive.ts
+++ src/app/structural-directive/structural-directive.directive.ts
@@ -5,12 +5,13 @@ import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 })
 
 // Structural diretive worki opposite of ngIf 
+//Set is used so that when ever variable's true/false value change, this direcive is called
 export class StructuralDirectiveDirective {
   @Input() set appStructuralDirective(condition : boolean){
-    if(!condition){
+    if(!condition){ //Display the data if condition is value
       this.viewContainerRef.createEmbeddedView(this.templateRef);
     }
-    else{
+    else{ //Hides dayta if condition is false
       this.viewContainerRef.clear();
     }
   }
